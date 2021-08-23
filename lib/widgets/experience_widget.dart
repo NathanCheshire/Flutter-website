e75f012e72bc8ac6c14488afe29b6911c4d7c6ad
Nathan <nathan.vincent.2.718@gmail.com>
2021-08-22 21:47:46 -0500

    move html/css to above flutter

--- lib/widgets/experience_widget.dart
+++ lib/widgets/experience_widget.dart
@@ -462,6 +462,87 @@ class ExperienceSection extends StatelessWidget {
               children: [
                 Center(
                   child: SvgPicture.asset(
+                    "assets/Html.svg",
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * .75,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Text(
+                        "Experience using HTML(5) for general purpose front-end" +
+                            " development. Understanding of language and how to write" +
+                            " properly for integration with CSS.",
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.robotoCondensed(
+                          textStyle: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: SvgPicture.asset(
+                    "assets/Css.svg",
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * .75,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Text(
+                        "Experience using CSS to decorate HTML and style elements." +
+                            " Experience making animations and complex designs using CSS." +
+                            " Experience using CSS in all three possible ways: internal," +
+                            " external, and inline.",
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.robotoCondensed(
+                          textStyle: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: SvgPicture.asset(
                     "assets/Flutter.svg",
                     width: 100,
                     height: 100,
@@ -559,87 +640,6 @@ class ExperienceSection extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 30),
                     child: Text(
                         "Experience using Node and npm for their respective purposes such as client/server programming for native/web apps.",
-                        textAlign: TextAlign.left,
-                        style: GoogleFonts.robotoCondensed(
-                          textStyle: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        )),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: SvgPicture.asset(
-                    "assets/Html.svg",
-                    width: 100,
-                    height: 100,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * .75,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Text(
-                        "Experience using HTML(5) for general purpose front-end" +
-                            " development. Understanding of language and how to write" +
-                            " properly for integration with CSS.",
-                        textAlign: TextAlign.left,
-                        style: GoogleFonts.robotoCondensed(
-                          textStyle: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        )),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: SvgPicture.asset(
-                    "assets/Css.svg",
-                    width: 100,
-                    height: 100,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width * .75,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Text(
-                        "Experience using CSS to decorate HTML and style elements." +
-                            " Experience making animations and complex designs using CSS." +
-                            " Experience using CSS in all three possible ways: internal," +
-                            " external, and inline.",
                         textAlign: TextAlign.left,
                         style: GoogleFonts.robotoCondensed(
                           textStyle: TextStyle(
