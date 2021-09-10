ca47fa1b999c866d384ccc29ef3cd610413667d9
Nathan <nathan.vincent.2.718@gmail.com>
2021-09-10 15:33:48 -0500

    add contact cards

--- lib/widgets/contact_widget.dart
+++ lib/widgets/contact_widget.dart
@@ -30,19 +30,25 @@ class ContactSection extends StatelessWidget {
                     "Personal: Nathan.Vincent.2.718@gmail.com\nBusiness: Nate@NathanCheshire.com",
                 url: "mailto:Nate@NathanCheshire.com",
                 excessIconPadding: 20,
+                color1: Color(0xFF5478FF),
+                color2: Color(0xFF8BC8FF),
               ),
               buildClickableContact(
                 icon: "assets/Github.svg",
                 text: "GitHub.com/NathanCheshire",
                 url: "GitHub.com/NathanCheshire",
                 excessIconPadding: 0,
+                color1: Color(0xFF8879F7),
+                color2: Color(0xFFD6ABFF),
               ),
               buildClickableContact(
                 icon: "assets/Discord.svg",
                 text: "Natche#8845",
                 url: "https://discord.com/app",
                 excessIconPadding: 0,
-              )
+                color1: Color(0xFFFF8DD4),
+                color2: Color(0xFFFFBEC1),
+              ),
             ],
           ),
         ),
@@ -57,12 +63,16 @@ class buildClickableContact extends StatelessWidget {
   final String text;
   final String url;
   final double excessIconPadding;
+  final Color color1;
+  final Color color2;
 
   const buildClickableContact(
       {required this.icon,
       required this.text,
       required this.url,
-      required this.excessIconPadding});
+      required this.excessIconPadding,
+      required this.color1,
+      required this.color2});
 
   Future<void> _launchInBrowser(String url) async {
     if (await canLaunch(url)) {
@@ -75,8 +85,6 @@ class buildClickableContact extends StatelessWidget {
     }
   }
 
-  //todo make these pretty cards that are spaced un evenenly
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -86,36 +94,52 @@ class buildClickableContact extends StatelessWidget {
         }
       }),
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          children: [
-            Stack(
-              alignment: Alignment.center,
+        padding: const EdgeInsets.all(40.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            gradient: LinearGradient(
+              colors: [
+                color1,
+                color2,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Wrap(
+              alignment: WrapAlignment.center,
               children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: excessIconPadding),
+                      child: SvgPicture.asset(
+                        icon,
+                        width: 75,
+                        height: 75,
+                      ),
+                    ),
+                  ],
+                ),
                 Padding(
-                  padding: EdgeInsets.only(top: excessIconPadding),
-                  child: SvgPicture.asset(
-                    icon,
-                    width: 75,
-                    height: 75,
-                  ),
+                  padding: const EdgeInsets.only(left: 20.0, top: 30.0),
+                  child: Text(text,
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.robotoCondensed(
+                        textStyle: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      )),
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 30.0),
-              child: Text(text,
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.robotoCondensed(
-                    textStyle: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  )),
-            ),
-          ],
+          ),
         ),
       ),
     );
